1fcd87b67833369a1bdb0b5d6914ced20d2c0d1a
Stanly Lee <stanly.lns@gmail.com>
2013-12-25 17:19:21 +0800

    init commit

---
+++ src/com/mooStan/typingcrush/TypingCrush.java
@@ -0,0 +1,15 @@
+package com.mooStan.typingcrush;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TypingCrush extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_typing_crush);
+	}
+
+}
---
+++ src/com/mooStan/typingcrush/serverComm.java
@@ -0,0 +1,5 @@
+package com.mooStan.typingcrush;
+
+public class serverComm {
+
+}
