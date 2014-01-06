42289a8ad8d17324ab448737042930ad4a85dbb6
Stanly Lee <stanly.lns@gmail.com>
2014-01-06 20:59:16 +0800

    day 13
    released V1

--- src/com/mooStan/typingcrush/TypingCrush.java
+++ src/com/mooStan/typingcrush/TypingCrush.java
@@ -28,6 +28,27 @@ public class TypingCrush extends Activity {
 		globalVariable = (GlobalVars) getApplicationContext();
 		
 		initSetup.basicDetection();
+		
+		/*
+		 * PackageInfo info;
+		try {
+		    info = getPackageManager().getPackageInfo("com.mooStan.typingcrush", PackageManager.GET_SIGNATURES);
+		    for (Signature signature : info.signatures) {
+		        MessageDigest md;
+		        md = MessageDigest.getInstance("SHA");
+		        md.update(signature.toByteArray());
+		        String something = new String(Base64.encode(md.digest(), 0));
+		        //String something = new String(Base64.encodeBytes(md.digest()));
+		        Log.e("hash key", something);
+		    }
+		} catch (NameNotFoundException e1) {
+		    Log.e("name not found", e1.toString());
+		} catch (NoSuchAlgorithmException e) {
+		    Log.e("no such an algorithm", e.toString());
+		} catch (Exception e) {
+		    Log.e("exception", e.toString());
+		}
+		 */
 	}
 
 	@Override
